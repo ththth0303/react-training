e789be8005047c2e5c5b1fba787ab3f2ec46f53d
ththth0303 <trungthangngo@gmail.com>
2017-09-04 01:36:55 +0700

    todo

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -4,7 +4,9 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: props.value || ''
+            value: props.value || '',
+            isCreating: false,
+            err: false,
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -18,19 +20,30 @@ export default class App extends React.Component {
         return this.state.value;
     }
 
+    cancel() {
+        this.setState({value: '', err: false, isCreating: false});
+    }
+
+    checkEnter(e) {
+        if (e.keyCode === 13) {
+            this.props.addTodo(this.getValue());
+        }
+    }
+
     render () {
         if(this.state.isCreating) {
             return (
                 <div id="content">
-                    <input className="name" paclehodel="new user" onChange={this.handleChange} value={this.state.value} ref={(c) => {this.todo = c;}}></input>
-                    <button className="btn" onClick={() => this.props.addTodo(this.getValue())}>Save</button>
-                    <button className="btn" onClick={() => this.setState({isCreating: false})}>Cancel</button>
+                    <input className="form-control" autoFocus onChange={this.handleChange} value={this.state.value} ref={(c) => {this.todo = c;}} onKeyUp={(e) => this.checkEnter(e)} focus></input>
+                    <button className="btn btn-primary" onClick={() => this.props.addTodo(this.getValue())}>Save</button>
+                    <button className="btn btn-warning" onClick={() => this.cancel()}>Cancel</button>
+                    {this.state.err ? <span>không được để trống</span> : null}
                 </div>
             )
         }
         return (
             <div id="content">
-                <button className="btn" onClick={() => this.setState({isCreating: true}) } onChange={this.handleChange}>New User</button>
+                <button className="btn btn-primary" onClick={() => this.setState({isCreating: true}) } onChange={this.handleChange}>New User</button>
             </div>
         )
     }
--- src/components/list/index.js
+++ src/components/list/index.js
@@ -3,28 +3,37 @@ import Item from './item';
 import Filter from '../filter';
 import Input from '../input';
 import _map from 'lodash/map';
+import _findIndex from 'lodash/findIndex';
+import './list.scss';
+import {fetchTodoList, createTodo, updateTodo, deleteTodo} from '../api';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            todos: [
-                {id: 1, name: 'Test todo 1', completed: false},
-                {id: 2, name: 'Test todo 2', completed: false},
-                {id: 3, name: 'Test todo 3', completed: true},
-                {id: 4, name: 'Test todo 4', completed: false},
-                {id: 5, name: 'Test todo 5', completed: false},
-                {id: 6, name: 'Test todo 6', completed: true},
-                {id: 7, name: 'Test todo 7', completed: false}
-            ],
+            todos: [],
             filter: 'SHOW_ALL',
-            value: ''
+            value: '',
+            edit: null
         }
         this.onChangeFilter = this.onChangeFilter.bind(this);
         this.addTodo = this.addTodo.bind(this);
+        this.deleteTodo = this.deleteTodo.bind(this);
+        this.changeStatus = this.changeStatus.bind(this);
+        this.editItem = this.editItem.bind(this);
+        this.updateItem = this.updateItem.bind(this);
+        this.onBlur = this.onBlur.bind(this);
+        this.addTodoUpdate = this.addTodoUpdate.bind(this);
     }
+
+    componentWillMount() {
+        fetchTodoList((response) => {
+            this.setState({todos: response.data})
+        });
+    }
+
     onChangeFilter(filter) {
-        this.setState({filter: filter});
+        this.setState({filter: filter, edit: null});
     }
 
     filterTodo() {
@@ -41,34 +50,90 @@ export default class App extends React.Component {
     }
 
     addTodo(e) {
+        e = {name: e, completed: false};
+        createTodo(e, this.addTodoUpdate);
+    }
+
+    addTodoUpdate(response) {
+            console.log(response);
+            let todo = {
+                    id: response.data.id,
+                    name: response.data.name,
+                    completed: response.data.completed,
+                }
         const todos = this.state.todos;
-        let ids = _map(todos, 'id');
-        let max = Math.max(...ids);
-        todos.push({
-            id: max+1,
-            name: e,
-            completed: false
-        });
-        this.setState({todos});
-        this.todo.setState({value: ''});
+            todos.push(todo);
+            this.setState({todos});
+            this.todo.setState({value: '', err:false, isCreating: false});
+    }
+
+
+    deleteTodo(id) {
+        const todos = this.state.todos;
+        let filter = todos.filter(t => t.id !== id);
+        this.setState({todos: filter});
+    }
+
+    // changeStatus(id) {
+    //     const todos = this.state.todos;
+    //     let index = _findIndex(todos, {id});
+    //     if (index !== -1) {
+    //         let todo = todos[index];
+    //         todo.completed = !todo.completed;
+    //         todos[index] = todo;
+    //         updateTodo(todo, this.changeStatusUpdate);
+    //         // this.setState({todos});
+    //     }
+    // }
+
+    changeStatusUpdate(response) {
+        console.log(response);
+    }
+
+    editItem(item) {
+        this.setState({edit: item.id});
+    }
+
+    updateItem (id, text, e) {
+        const todos = this.state.todos;
+        let index = _findIndex(todos, {id});
+        if (index !== -1) {
+            let todo = todos[index];
+            todo.name = text;
+            todos[index] = todo;
+            this.setState({todos});
+            if (e.keyCode === 13) {
+                this.onBlur();
+            }
+        }
+    }
+
+    onBlur () {
+        this.setState({edit: null})
     }
 
     render () {
 
         return (
-            <div>
-                <Filter onClick={this.onChangeFilter}/>
-                <table>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                     </tr>
-                    {this.filterTodo().map((item) =>
-                        <Item id={item.id} name={item.name} key={item.id}/>
-                    )}
+            <div className="content">
+                <Filter onClick={this.onChangeFilter} filter={this.state.filter}/>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th className="name">Name</th>
+                            <th>Completed</th>
+                            <th colSpan="2">Action</th>
+                         </tr>
+                    </thead>
+                    <tbody>
+                        {this.filterTodo().length ? this.filterTodo().map((item) =>
+                            <Item item={item} key={item.id} delete={() => this.deleteTodo(item.id)} changeStatus={this.updateItem} editing={this.state.edit == item.id} ref={(c) => {this.item = c;}} editItem={this.editItem} updateItem={this.updateItem} onBlur={this.onBlur}/>
+                        ) : <tr><td colSpan="4">No Data</td></tr>}
+                    </tbody>
                 </table>
                 <Input addTodo={this.addTodo} value={this.state.value} ref={(c) => {this.todo = c;}}/>
-            </div>    
+            </div>
         )
     }
 }
